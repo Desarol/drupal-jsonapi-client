762b5a2659731ac5ca4b3cb3b08944839bdf3a7d
Auspicus <elijahdisch@gmail.com>
2019-05-26 11:20:45 -0400

    2.0.0: updated examples

---
+++ examples/FetchAndEditExistingEntity.js
@@ -0,0 +1,14 @@
+import { Client } from 'drupal-json-client'
+
+const doRequest = async () => {
+  const client = new Client({
+    transport: fetch,
+    baseUrl: 'https://www.example.com',
+    sendCookies: true, // use this when running code on the same origin as Drupal
+  })
+
+  const entity = await client.getEntity('node', 'article', 'uuid')
+  entity.setAttribute('title', 'Drupal JSON:API rocks!')
+  await client.send(entity.toPatchRequest())
+}
+doRequest()
