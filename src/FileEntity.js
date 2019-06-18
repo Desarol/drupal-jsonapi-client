eff69b4b099778df10e9bfc02f3234065e86791d
Auspicus <elijahdisch@gmail.com>
2019-06-18 14:04:50 -0500

    3.x: added back node.js polyfill for File and FileReader

--- src/FileEntity.js
+++ src/FileEntity.js
@@ -1,17 +1,37 @@
 import GlobalClient from './GlobalClient'
 import Entity from './Entity'
 
+const setPolyfill = () => {
+  global.File = function File() {}
+  global.FileReader = function FileReader() {}
+}
+
+// File class does not exist in node.js
+if (File === undefined || FileReader === undefined) {
+  setPolyfill()
+}
+
 // Named FileEntity to avoid namespace collisions in browsers
 export default class FileEntity extends Entity {
   static async Upload(fileOrBinary, name, entityType, entityBundle, fieldName) {
-    const fileName = name || fileOrBinary.name
-    const binary = await new Promise((resolve) => {
-      const fr = new FileReader();
-      fr.onload = (event) => {
-        resolve(event.target.result);
-      };
-      fr.readAsArrayBuffer(fileOrBinary);
-    })
+    let fileName = name
+    let binary
+
+    if (fileOrBinary instanceof File) {
+      binary = await new Promise((resolve) => {
+        const fr = new FileReader();
+        fr.onload = (event) => {
+          resolve(event.target.result);
+        };
+        fr.readAsArrayBuffer(fileOrBinary);
+      })
+
+      if (name === null) {
+        fileName = fileOrBinary.name
+      }
+    } else {
+      binary = fileOrBinary
+    }
 
     const response = await GlobalClient.send(
       new Request(`/jsonapi/${entityType}/${entityBundle}/${fieldName}`, {
